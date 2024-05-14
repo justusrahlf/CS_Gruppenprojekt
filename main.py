098f965574a7306b56e59207240ecea9a5f9c6c8
Moritcs <163539424+Moritcs@users.noreply.github.com>
2024-05-14 18:30:16 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -3,12 +3,62 @@ from data_helper import *
 from calculator import calculate_macronutrients
 from api import answer
 from matplotlib import pyplot as plt
+import base64
 
 # Set page title and subtitle
 st.title("Macronutrient planner")
 st.markdown("University of St. Gallen - Computer Science 9.1")
 st.markdown(
-    "Creators: Theo Rostalski, Felix Höner, Justus Rahlf, Loredana Salvi, Moritz Müller, Richard Blanc, Fabian von Widdern"
+    "Creators: Theo Rostalski, Felix Höner, Justus Rahlf, Loredana Salvi, Moritz Müller, Richard Blanc, Fabian von Widdern")
+
+# Function to load and encode the image to base64
+def get_image_as_base64(image_path):
+    with open(image_path, "rb") as img_file:
+        return base64.b64encode(img_file.read()).decode()
+
+# Load the image and encode it to base64
+image_base64 = get_image_as_base64("logo.png")
+
+# HTML and CSS to position the logo in the upper right corner responsively
+st.markdown(
+    f"""
+    <style>
+    .logo-container {{
+        position: absolute;
+        top: -180px;
+        right: 10px;
+        z-index: 1;
+    }}
+    .logo {{
+        width: 100px;
+        max-width: 100%;
+        height: auto;
+    }}
+    @media (max-width: 600px) {{
+        .logo-container {{
+            top: 10px;
+            right: 10px;
+        }}
+        .logo {{
+            width: 80px;
+        }}
+    }}
+    @media (max-width: 400px) {{
+        .logo-container {{
+            top: 5px;
+            right: 5px;
+        }}
+        .logo {{
+            width: 60px;
+        }}
+    }}
+    
+    </style>
+    <div class="logo-container">
+        <img src="data:image/png;base64,{image_base64}" class="logo">
+    </div>
+    """,
+    unsafe_allow_html=True
 )
 
 # Create input fields for calculating the macronutrient distribution
@@ -29,18 +79,24 @@ activity_level = st.selectbox(
 )
 goal = st.selectbox("Ziel", goals, index=goals.index(user["goal"]))
 
-# Display an empty table for results using placeholder
-empty_result_table = {
-    "Kalorien": 0,
-    "Protein (g)": 0,
-    "Fett (g)": 0,
-    "Kohlenhydrate (g)": 0,
-}
-st.header("Nährwerte")
-result_table_placeholder = st.empty()
-result_table_placeholder.table(empty_result_table)
+# Create columns for layout
+col1, col2 = st.columns([1, 1])
 
-piechart = st.empty()
+# Display an empty table for results using placeholder in column 1
+with col1:
+    st.header("Nährwerte")
+    empty_result_table = {
+        "Kalorien": 0,
+        "Protein (g)": 0,
+        "Fett (g)": 0,
+        "Kohlenhydrate (g)": 0,
+}
+    result_table_placeholder = st.empty()
+    result_table_placeholder.table(empty_result_table)
+
+# Create an empty space where we put the pie chart in column 2
+with col2:
+    piechart = st.empty()
 
 # Create an empty space where we put the meal
 menu_vorschlag = st.empty()
@@ -66,41 +122,20 @@ if st.button("Berechne"):
         
         # Data for Graph 1
         nutritional_value = ["Kohlenhydrate", "Proteine", "Fette"]
-        weights = [result["Kohlenhydrate"],result["Proteine"], result["Fette"] ]
+        weights = [result["Kohlenhydrate"],result["Proteine"], result["Fette"] ] #soll sich anpassen an die Berechnung 
         
-        # Data for Graph 2
-        own_calories = result["Kalorien"]
-        durchschnitt_kalorien_schweiz = 2573  # Durchschnittliche tägliche Kalorienaufnahme laut Statista
-        kalorien_daten = [own_calories, durchschnitt_kalorien_schweiz]
-        labels = ['Eigene Kalorien', 'Durchschnitt Schweiz']
-       
-        # Creating a figure with 2 subplots
-        fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 5)) 
-        fig.subplots_adjust(wspace=0.5)
-       
-        # Determine the font size of the labels
-        fontsize_for_labels = 10 
- 
-        # Creating the Piechart
-        space, texts, autotexts = ax1.pie(weights, labels=nutritional_value, autopct = "%1.2f%%", startangle = 90, shadow = True, explode = (0.08, 0.08, 0.08))
-        ax1.set_title('Aufteilung der Nährwerte', fontweight='bold')
-       
+        fig, ax = plt.subplots()
+        space, texts, autotexts = ax.pie(weights, labels=nutritional_value, autopct = "%1.2f%%", startangle = 90, shadow = True, explode = (0.08, 0.08, 0.08),radius=0.8)
+        
         for text in texts:
-            text.set_fontsize(fontsize_for_labels)
+            text.set_fontsize(10)
             text.set_weight('bold')
-           
+            
         for autotext in autotexts:
             autotext.set_color('white')
-            autotext.set_fontsize(fontsize_for_labels)
+            autotext.set_fontsize(10)
             autotext.set_weight('bold')
-       
-        # Creating the Bar Chart
-        ax2.bar(labels, kalorien_daten, color = ['blue', 'green'])
-        ax2.set_title('Durchschnittlich weltweiter vergleich deiner Kalorien', fontweight = "bold") 
-        ax2.set_ylabel('Kalorien', fontdict={'weight': 'bold'}) #set_ylabel erwartet ein dic
-        ax2.set_xticklabels(labels,fontsize = fontsize_for_labels, fontweight = 'bold')
-      
-        #Showig both Graphs
+    
         st.pyplot(fig)
 
     # Ask Chatgpt
